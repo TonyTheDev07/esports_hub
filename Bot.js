8b9fdc02d001f913d42e24031d922424e76906f5
TonyTheDev07 <59507588+TonyTheDev07@users.noreply.github.com>
2020-02-22 10:18:52 +0530

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,30 +1,38 @@
-
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
 });
+logger.level = 'debug';
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+var bot = new Discord.Client({
+   token: 
+   autorun: true
 });
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.NjgwNDE4NzQ3MDU5NTM1ODk4.Xk_8QQ.-YbVcEqQyoUNJgtE76HvuimqQ4E);//where BOT_TOKEN is the
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: 673437537435648012,
+                    message: 'Pong!'
+                });
+            break;
+            
+         }
+     }
+});
